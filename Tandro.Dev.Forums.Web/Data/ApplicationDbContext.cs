051fe4a3cc48cbb3928bd0c0910edb66e31f32eb
tobiashungwe <tobias.hungwe@student.howest.be>
2021-06-06 14:15:22 +0200

    Add project files.

---
+++ Tandro.Dev.Forums.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tandro.Dev.Forums.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
